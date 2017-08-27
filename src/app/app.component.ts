a0cbf922c3e619d8fa5f04ff2836d2dea337787b
백명석(Myeongseok Baek)/Tech Infra 개발본부/SKP <ms.baek@sk.com>
2017-08-27 17:49:09 +0900

    hero object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`,
+  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'My First Angular App';
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
+export class Hero {
+  id: number;
+  name: string;
 }
