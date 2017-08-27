30056c09ee0c83f1beae92512d092769e8ee3bee
백명석(Myeongseok Baek)/Tech Infra 개발본부/SKP <ms.baek@sk.com>
2017-08-27 17:50:35 +0900

    Adding HTML with multi-line template strings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div><label>name: </label>{{hero.name}}</div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
