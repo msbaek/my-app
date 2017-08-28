4ef8db430db4e0e9a083935fe3abd276902eba78
백명석(Myeongseok Baek)/Tech Infra 개발본부/SKP <ms.baek@sk.com>
2017-08-28 09:00:05 +0900

    multiple components
    - move hero detail html to hero-detail
    - declare hero property
    - move Hero class to hero.ts
    - make hero property as input property in HeroDetailComponent
    - read comments on app.component.html(target input property...)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Hero} from './hero';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +11,7 @@ import {Component} from '@angular/core';
         <span class="badge">{{hero.id}}</span>{{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details !</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name">
-      </div>
-    </div>
+    <app-hero-detail [hero]="selectedHero"></app-hero-detail>
   `,
   styles: [`
     .selected {
@@ -100,8 +94,4 @@ export class AppComponent {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-}
-export class Hero {
-  id: number;
-  name: string;
 }
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Hero} from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
+  template: `
+    <div *ngIf="hero">
+      <h2>{{hero.name}} details !</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name">
+      </div>
+    </div>
+  `,
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+  // declare that hero is an input property
+  // by preceding it with the @Input decorator
+  @Input() hero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
