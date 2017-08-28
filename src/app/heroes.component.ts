8e9cc06186d13a864735556ee73650ff4123350d
백명석(Myeongseok Baek)/Tech Infra 개발본부/SKP <ms.baek@sk.com>
2017-08-28 16:33:02 +0900

    implement gotoDetail using Router

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from './hero';
 import {HeroService} from './hero.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -11,7 +12,10 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) {
+  constructor(
+    private router: Router,
+    private heroService: HeroService
+  ) {
   }
 
   ngOnInit(): void {
@@ -26,6 +30,7 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
 
-  gotoDetail() {
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
