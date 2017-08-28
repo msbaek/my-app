6fa649b12112b27b2f1b0c15d0a6eec7b5b2b879
백명석(Myeongseok Baek)/Tech Infra 개발본부/SKP <ms.baek@sk.com>
2017-08-28 15:55:26 +0900

    route for hero detail by id
    - move html to hero-detail.component.html(add button for goBack)
    - rxjs switchMap, ActivatedRoute paramMap to handle by Id
    - add route for by Id
    - add getHero by Id in HeroService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
         path: '',
         redirectTo: '/dashboard',
         pathMatch: 'full'
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
       }
     ])
   ],
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,18 +1,13 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+import {Location} from '@angular/common';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details !</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-      </div>
-    </div>
-  `,
+  templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
@@ -20,8 +15,19 @@ export class HeroDetailComponent implements OnInit {
   // by preceding it with the @Input decorator
   @Input() hero: Hero;
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.route.paramMap
+      .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 }
