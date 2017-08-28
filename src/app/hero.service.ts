94371d28bc24c2293b176b7d28c1c19c2a4cf368
백명석(Myeongseok Baek)/Tech Infra 개발본부/SKP <ms.baek@sk.com>
2017-08-28 22:46:39 +0900

    Get hero by id

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,21 +11,19 @@ export class HeroService {
   }
 
   getHeroes(): Promise<Hero []> {
+    // http.get returns RxJS Observable(https://angular.io/tutorial/toh-pt6#observables)
     return this.http.get(this.heroesUrl)
       .toPromise()
       .then(response => response.json().data as Hero[])
       .catch(this.handleError);
   }
 
-  getHeroesSlowly(): Promise<Hero[]> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(this.getHeroes()), 2000);
-    });
-  }
-
   getHero(id: number) {
-    return this.getHeroes()
-      .then(heroes => heroes.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json().data as Hero)
+      .catch(this.handleError);
   }
 
   private handleError(error: any): Promise<any> {
